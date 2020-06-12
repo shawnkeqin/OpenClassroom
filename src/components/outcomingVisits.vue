216e58c4b51b12ae09340f46e1cab2502565a134
Dan Tran <picasdan99@gmail.com>
2020-06-12 16:34:15 +0800

    load information for my visits page

--- src/components/outcomingVisits.vue
+++ src/components/outcomingVisits.vue
@@ -1,26 +1,54 @@
 <template>
-<div>
-<h1>Outcoming Visits</h1>
-<br />
-        <seminarCardRequest :seminar="{module_code: 'YSC4211B', title: 'Adv Topics Molecular, Cell & Developmental Bio: Stem Cells', start: '0900', end: '1030', date: '2020-06-08', location_code: 'Y-CR20', desc: 'loremipsum1', tags: ['TAG1', 'TAG2', 'TAG3'], instructor: {name: 'Matthew Stamp', profilePic: 'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1627&q=80'} }" :requestStatus="''"/>
-  <seminarCardRequest :seminar="{module_code: 'YSC3237', title: 'Introduction to Modern Algebra', start: '0900', end: '1030', date: '2020-06-08', location_code: 'Y-CR20', desc: 'loremipsum2', tags: ['TAG1', 'TAG2', 'TAG3'], instructor: {name: 'Matthew Stamp', profilePic: 'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1627&q=80'} }" :requestStatus="'pending'"/>
-  <seminarCardRequest :seminar="{module_code: 'YSC2222', title: 'Organic Chemistry Laboratory', start: '0900', end: '1030', date: '2020-06-08', location_code: 'Y-CR20', desc: 'loremipsum3', tags: ['TAG3', 'TAG4', 'TAG5'], instructor: {name: 'NAME HERE'} }" :requestStatus="'accepted'"/>
-  <seminarCardRequest :seminar="{module_code: 'YSC3253', title: 'Coral Reef Ecology and Environmental Change', start: '0900', end: '1030', date: '2020-06-08', location_code: 'Y-CR20', desc: 'loremupsum4', tags: ['TAG6', 'TAG7', 'TAG8'], instructor: {name: 'NAME HERE'} }" :requestStatus="'declined'"/>
-</div>
+  <div>
+    <h1>Outcoming Visits</h1>
+    <br />
+    <!-- <div>{{ myVists }}</div> -->
+    <myVisitCard v-for="visit in myVists" :visit="visit" :key="visit.id" />
+  </div>
 </template>
 
+
 <script>
-import seminarCardRequest from "./seminarCardRequest"
+import myVisitCard from "./myVisitCard";
+import gql from 'graphql-tag';
+
 export default {
   name: "observelog",
   components: {
-    seminarCardRequest
-  }
-
-}
-
+    myVisitCard
+  },
+  // computed: {
+  //   myVists() {
+  //     return this.$store.state.myVists;
+  //   }
+  // },
+  data() {
+    return {
+      myVisits: []
+    }
+  },
+  apollo: {
+    myVists: {
+      query: gql`
+        query getMyVisits($visitor_id: String!) {
+          visit(where: { visitor_id: { _eq: $visitor_id } }) {
+            id
+            seminar_id
+            visit_status
+            visitor_id
+          }
+        }`,
+      variables: {
+        visitor_id: "yncsjm"
+      },
+      update: data => data.visit
+    }
+  },
+  // create() {
+    // console.log('created');
+    // this.$store.dispatch("fetchMyVisits", { visitor_id: "yncsjm" });
+  // }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
