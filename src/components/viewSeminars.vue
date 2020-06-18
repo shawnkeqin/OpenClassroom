f3baa8577e54dcf0232af940751f64ce308de5e2
devtoohard <e0052482@u.nus.edu>
2020-06-18 12:48:52 +0800

    eslint

--- src/components/viewSeminars.vue
+++ src/components/viewSeminars.vue
@@ -6,65 +6,74 @@
       size="large"
       v-model="title"
     />
-    <br/>
-       <br/>
+    <br />
+    <br />
 
-<p>&nbsp;</p>
-<p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
 
-
-
-       
-
-     <div align="right">
-            <h2 align="left">Results</h2>
-  <div align="left" class="list-of-seminars">
-<seminarCardRequest  v-for="seminar in seminar"
-      :key="seminar.id"
-      :seminar="seminar"
-      class="seminar-item" >
-  </seminarCardRequest >
-   </div>
-  <a-card style="width: 300px">
-     <a-form  :form="form" >
- <h4 align="center">Filter</h4>
-  <p align="center">Date Range</p>
- <!--  <a-range-picker
+    <div align="right">
+      <h2 align="left">Results</h2>
+      <div align="left" class="list-of-seminars">
+        <seminarCardRequest
+          v-for="seminar in seminar"
+          :key="seminar.id"
+          :seminar="seminar"
+          class="seminar-item"
+        >
+        </seminarCardRequest>
+      </div>
+      <a-card style="width: 300px">
+        <a-form :form="form">
+          <h4 align="center">Filter</h4>
+          <p align="center">Date Range</p>
+          <!--  <a-range-picker
       :default-value="[moment('2015/01/01', dateFormat), moment('2015/01/01', dateFormat)]"
       :format="dateFormat"
     /> -->
-    <br />
-     <p align="center">Timings</p>
-     <p align="center">Start - End</p>
-     <p align="center">
-      <a-time-picker :open.sync="open2" >
-      <a-button slot="addon" size="small" type="primary" @click="handleClose">
-        Ok
-      </a-button>
-    </a-time-picker>
- </p>
+          <br />
+          <p align="center">Timings</p>
+          <p align="center">Start - End</p>
+          <p align="center">
+            <a-time-picker :open.sync="open2">
+              <a-button
+                slot="addon"
+                size="small"
+                type="primary"
+                @click="handleClose"
+              >
+                Ok
+              </a-button>
+            </a-time-picker>
+          </p>
 
-    <p align="center">Instructor</p>
-     <a-form-item>
-     <a-select v-model="name">
-    <a-select-option v-for="i in 25" :value="(i + 9).toString(36) + i" :key="(i + 9).toString(36) + i">
-      {{ (i + 9).toString(36) + i }}
-    </a-select-option>
-  </a-select>
-  </a-form-item>
-     <p align="center">Tags</p>
-       <a-select v-model="tags" mode="tags" style="width: 100%" placeholder="Tags Mode">
-    <a-select-option v-for="i in 25" :key="(i + 9).toString(36) + i">
-      {{ (i + 9).toString(36) + i }}
-    </a-select-option>
-  </a-select>
-  </a-form>
-  </a-card>
-
-   
- 
-      </div>
-  <!--  <a-card title="Search Courses" style="width: 1400px">
+          <p align="center">Instructor</p>
+          <a-form-item>
+            <a-select v-model="name">
+              <a-select-option
+                v-for="i in 25"
+                :value="(i + 9).toString(36) + i"
+                :key="(i + 9).toString(36) + i"
+              >
+                {{ (i + 9).toString(36) + i }}
+              </a-select-option>
+            </a-select>
+          </a-form-item>
+          <p align="center">Tags</p>
+          <a-select
+            v-model="tags"
+            mode="tags"
+            style="width: 100%"
+            placeholder="Tags Mode"
+          >
+            <a-select-option v-for="i in 25" :key="(i + 9).toString(36) + i">
+              {{ (i + 9).toString(36) + i }}
+            </a-select-option>
+          </a-select>
+        </a-form>
+      </a-card>
+    </div>
+    <!--  <a-card title="Search Courses" style="width: 1400px">
      <form @submit.prevent="submit">
         <fieldset>
           <input type="text" placeholder="Seminar ID" v-model="seminar_id" />
@@ -80,7 +89,7 @@
         </fieldset>
         <input class="button-primary" type="submit" value="Send" />
       </form> -->
-   <!--    <a-descriptions title="Search Fields">
+    <!--    <a-descriptions title="Search Fields">
     <a-descriptions-item label="Seminar ID">
    <input v-model="seminar_id" placeholder="Seminar ID"  />
     </a-descriptions-item>
@@ -105,34 +114,33 @@
  
   </a-descriptions>
 
---> 
-      
- 
-   <!-- <seminar-item
+-->
+
+    <!-- <seminar-item
       v-for="seminartest in seminartest"
       :key="seminartest.seminar_id"
       :seminartest="seminartest"
       class="seminar-item"
     > -->
-     
-  <!-- <seminarCardRequest :seminar="{module_code: 'YSC4211B', title: 'Adv Topics Molecular, Cell & Developmental Bio: Stem Cells', start: '0900', end: '1030', date: '2020-06-08', location_code: 'Y-CR20', desc: 'loremipsum1', tags: ['TAG1', 'TAG2', 'TAG3'], instructor: {name: 'Matthew Stamp', profilePic: 'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1627&q=80'} }" :requestStatus="''"/>  -->
-   
-   <!-- </seminar-item>  -->
- 
-   <!-- <p>{{seminartest}}</p> 
+
+    <!-- <seminarCardRequest :seminar="{module_code: 'YSC4211B', title: 'Adv Topics Molecular, Cell & Developmental Bio: Stem Cells', start: '0900', end: '1030', date: '2020-06-08', location_code: 'Y-CR20', desc: 'loremipsum1', tags: ['TAG1', 'TAG2', 'TAG3'], instructor: {name: 'Matthew Stamp', profilePic: 'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1627&q=80'} }" :requestStatus="''"/>  -->
+
+    <!-- </seminar-item>  -->
+
+    <!-- <p>{{seminartest}}</p> 
       
    <seminarCardRequest :seminar="{module_code: 'YSC3237', title: 'Introduction to Modern Algebra', start: '0900', end: '1030', date: '2020-06-08', location_code: 'Y-CR20', desc: 'loremipsum2', tags: ['TAG1', 'TAG2', 'TAG3'], instructor: {name: 'Matthew Stamp', profilePic: 'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1627&q=80'} }" :requestStatus="'pending'"/>
   <seminarCardRequest :seminar="{module_code: 'YSC2222', title: 'Organic Chemistry Laboratory', start: '0900', end: '1030', date: '2020-06-08', location_code: 'Y-CR20', desc: 'loremipsum3', tags: ['TAG3', 'TAG4', 'TAG5'], instructor: {name: 'NAME HERE'} }" :requestStatus="'accepted'"/>
   <seminarCardRequest :seminar="{module_code: 'YSC3253', title: 'Coral Reef Ecology and Environmental Change', start: '0900', end: '1030', date: '2020-06-08', location_code: 'Y-CR20', desc: 'loremupsum4', tags: ['TAG6', 'TAG7', 'TAG8'], instructor: {name: 'NAME HERE'} }" :requestStatus="'declined'"/> 
- --> 
+ -->
   </div>
 </template>
 
 <script>
-import moment from 'moment';
+import moment from "moment";
 //import seminarItem from "./seminarItem";
 import gql from "graphql-tag";
-import seminarCardRequest from "./seminarCardRequest"
+import seminarCardRequest from "./seminarCardRequest";
 //import seminarItem from './seminarItem'
 
 export default {
@@ -140,14 +148,13 @@ export default {
   components: { seminarCardRequest },
   data() {
     return {
-    form: this.$form.createForm(this, { name: 'form' }),
-     open2: false,
-     title: "",
-     name: "",
-     tags: []  
-    
-     
-  /*    seminar: [], 
+      form: this.$form.createForm(this, { name: "form" }),
+      open2: false,
+      title: "",
+      name: "",
+      tags: []
+
+      /*    seminar: [], 
       course_title: "",
       location: "",
       date: "",
@@ -156,14 +163,14 @@ export default {
       dateFormat: 'YYYY/MM/DD',
       monthFormat: 'YYYY/MM',
       dateFormatList: ['DD/MM/YYYY', 'DD/MM/YY'], */
-       // seminar: []
+      // seminar: []
     };
   },
   apollo: {
-  //  seminartest: {
-  //    query: GET_SEMINARS
-  //  },
- /*   seminartest: {
+    //  seminartest: {
+    //    query: GET_SEMINARS
+    //  },
+    /*   seminartest: {
     query: gql`
 query findSeminar($seminar_id: String!, $course_title: String!, $location: String!, $date: String!,  $start: String! , $end: String!)  {
   seminartest(where: {_or: [{location: {_similar: $location}}, {course_title: {_similar: $course_title}},{seminar_id: {_similar: $seminar_id}},  {date: {_eq: $date}},  {start: {_eq: $start}}, {end: {_eq: $end}}] }) {
@@ -179,43 +186,44 @@ query findSeminar($seminar_id: String!, $course_title: String!, $location: Strin
 
 `
 */
-seminar: {
-query: gql`
-query MyQuery($title: String!, $name: String!) {
-  seminar(where: {_or: [{course_group: {course: {title: {_similar: $title}}}}, {course_group: {faculty: {name: {_similar: $name}}}}]} ) {
-    date
-    start
-    end
-    id
-    location {
-      code
-    }
-    course_group {
-      course {
-        title
-        module_code
+    seminar: {
+      query: gql`
+        query MyQuery($title: String!, $name: String!) {
+          seminar(
+            where: {
+              _or: [
+                { course_group: { course: { title: { _similar: $title } } } }
+                { course_group: { faculty: { name: { _similar: $name } } } }
+              ]
+            }
+          ) {
+            date
+            start
+            end
+            id
+            location {
+              code
+            }
+            course_group {
+              course {
+                title
+                module_code
+              }
+            }
+            is_open
+          }
+        }
+      `,
+      variables() {
+        return {
+          title: this.title,
+          name: this.name
+        };
       }
     }
-    is_open
-  }
-}`
-, 
-variables() {
-  return {
-    title: this.title ? `%${this.title}%` : "%",
-    name: this.name || "%" 
-  
-
-
-
-   
-  }
-}
-}
-
   },
   methods: {
-  onSearch(value) {
+    onSearch(value) {
       console.log(value);
     },
     moment,
@@ -225,8 +233,8 @@ variables() {
     handleClose() {
       this.open = false;
       this.open2 = false;
-    },
-  } 
+    }
+  }
 };
 </script>
 
@@ -241,6 +249,5 @@ variables() {
 
 .filter {
   position: absolute;
-  
 }
 </style>
