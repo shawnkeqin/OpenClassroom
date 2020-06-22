be03ec664d4d3f9f9d294bb3595025d29123f12d
Dan Tran <picasdan99@gmail.com>
2020-06-22 22:31:02 +0800

    Redesign MyVisitCard

--- src/components/MyVisitsPage/MyVisitCard.vue
+++ src/components/MyVisitsPage/MyVisitCard.vue
@@ -10,88 +10,131 @@
       }}</span> -->
   <!--    <a-tag v-for="tag in seminar.tags" :key="tag">{{ tag }}</a-tag> -->
   <!-- </div> -->
-  <a-card
-    hoverable
-    style="width: 40rem; margin-bottom: 20px"
-    bodyStyle="padding: 10px"
-  >
-    <a-col :span="5">
-      <h5>{{ utils.date_format(seminar.date) }}</h5>
-      <h5>
+  <a-card hoverable style="width: 40rem; margin-bottom: 20px">
+    <a-row style="margin-bottom: 5px">
+      <h5 style="display: inline; font-weight: bold">
+        {{ utils.date_format(seminar.date) + " | " }}
+      </h5>
+      <h5 style="display: inline">
         {{
           utils.time_format(seminar.start) +
             " - " +
-            utils.time_format(seminar.end)
+            utils.time_format(seminar.end) +
+            " | "
         }}
       </h5>
-      <h5 style="padding-top: 10px">{{ seminar.location.full_name }}</h5>
-    </a-col>
-    <a-col :span="13" style="padding-right: 10px">
-      <!-- <a-row type="flex" style="align-items: center">
-        <a @click="handleAddToCalendar">Add to calendar</a>
-      </a-row> -->
-      <h3 style="display: inline-block; margin-bottom: 5px">{{ course.title }}</h3>
-      <h5 style="display: inline-block; padding-left: 5px">{{ seminar.module_code }}</h5>
-      <h4 style="margin-bottom: 5px">{{ seminar.title || 'This is seminar title' }}</h4>
-      <h5 class="truncate">{{ seminar.desc || course.desc || 'This is some seminar descriptions' }}</h5>
-      <a @click="isDescModalOn = true"
-        >View full course description and seminar details</a
-      >
-      <a-modal
-        v-model="isDescModalOn"
-        @ok="isDescModalOn = false"
-      >
-        <template slot="footer">
-          <a-button @click="isDescModalOn = false">Close</a-button>
-        </template>
-        <h4>Seminar description:</h4>
-        <p>{{ seminar.desc }}</p>
-        <h4>Course description:</h4>
-        <p>{{ course.desc }}</p>
-      </a-modal>
-    </a-col>
-    <a-col :span="6" type="flex" align="middle">
-      <a-button
-        @click="isCancelRequestModalOn = true"
-        block
-        type="primary"
-        ghost
-        >Cancel request</a-button
-      >
-      <a-modal
-        v-model="isCancelRequestModalOn"
-        @ok="handleCancelRequest"
-        title="You are about to cancel your visit request"
-      >
-        <template slot="footer">
-          <a-button key="cancel" @click="isCancelRequestModalOn = false"
-            >Cancel</a-button
+      <h6 style="display: inline">{{ seminar.location.full_name }}</h6>
+    </a-row>
+    <a-row style="margin-bottom: 20px">
+      <a-col :span="17" style="padding-right: 20px">
+        <a-row style="margin-bottom: 5px">
+          <h3 style="display: inline">
+            {{ course.title }}
+          </h3>
+          <p style="display: inline">
+            {{ seminar.module_code }}
+          </p>
+          <h4>{{ seminar.title || "This is seminar title" }}</h4>
+        </a-row>
+        <a-row style="margin-bottom: 10px">
+          <h5 class="truncate" style="margin-bottom: 0">
+            {{
+              seminar.desc || course.desc || "This is some seminar descriptions"
+            }}
+          </h5>
+          <a @click="isDescModalOn = true"
+            >View full course description and seminar details</a
           >
-          <a-button key="submit" @click="handleCancelRequest"
-            >Confirm cancel request</a-button
-          >
-        </template>
-        <h6>Your request message</h6>
-        <p>{{ visit.request_msg }}</p>
-        <h6>Instructor's response message</h6>
-        <p>{{ visit.response_msg }}</p>
-      </a-modal>
-      <div>
-        <template v-if="visit.visit_status === 'PENDING'">
-          <a-icon type="clock-circle" theme="filled" class="pending" />
-          <span class="request-status pending">Request pending</span>
-        </template>
-        <template v-else-if="visit.visit_status === 'ACCEPTED'">
-          <a-icon type="check-circle" theme="filled" class="accepted" />
-          <span class="request-status accepted">Request accepted</span>
-        </template>
-        <template v-else-if="visit.visit_status === 'DECLINED'">
-          <a-icon type="close-circle" theme="filled" class="declined" />
-          <span class="request-status declined">Request declined</span>
-        </template>
-        <template v-else />
-      </div>
-    </a-col>
+        </a-row>
+        <a-modal v-model="isDescModalOn" @ok="isDescModalOn = false">
+          <template slot="footer">
+            <a-button @click="isDescModalOn = false">Close</a-button>
+          </template>
+          <h4>Seminar description:</h4>
+          <p>{{ seminar.desc }}</p>
+          <h4>Course description:</h4>
+          <p>{{ course.desc }}</p>
+        </a-modal>
+        <h5>
+          {{ "Notes for visitors: " + (course_group.notes || "Some notes") }}
+        </h5>
+      </a-col>
+      <a-col :span="7" type="flex" align="bottom" style="display: inline-block">
+        <a-button
+          @click="isCancelRequestModalOn = true"
+          type="primary"
+          ghost
+          block
+          style="margin-bottom: 20px"
+          >Cancel request</a-button
+        >
+        <a-modal
+          v-model="isCancelRequestModalOn"
+          @ok="handleCancelRequest"
+          title="You are about to cancel your visit request"
+        >
+          <template slot="footer">
+            <a-button key="cancel" @click="isCancelRequestModalOn = false"
+              >Cancel</a-button
+            >
+            <a-button key="submit" @click="handleCancelRequest"
+              >Confirm cancel request</a-button
+            >
+          </template>
+          <h6>Your request message</h6>
+          <p>{{ visit.request_msg }}</p>
+          <h6>Instructor's response message</h6>
+          <p>{{ visit.response_msg }}</p>
+        </a-modal>
+        <div style="display: flex; justify-content: center">
+          <template v-if="visit.visit_status === 'PENDING'">
+            <a-icon
+              type="clock-circle"
+              theme="filled"
+              class="status-icon pending"
+            />
+            <h4 class="pending" style="margin-bottom: 0">
+              Request pending
+            </h4>
+          </template>
+          <template v-else-if="visit.visit_status === 'ACCEPTED'">
+            <div style="display: flex; align-items: center">
+              <a-icon
+                type="check-circle"
+                theme="filled"
+                class="status-icon accepted"
+              />
+            </div>
+            <div>
+              <h4 class="accepted" style="margin-bottom: 3px">
+                Request accepted
+              </h4>
+              <h5 class="accepted">
+                {{
+                  visit.time_responded &&
+                    utils.datetime_fromnow_format(visit.time_responded)
+                }}
+              </h5>
+            </div>
+          </template>
+          <template v-else-if="visit.visit_status === 'DECLINED'">
+            <a-icon
+              type="closed-circle"
+              theme="filled"
+              class="status-icon declined"
+            />
+            <h4 class="declined" style="margin-bottom: 0">
+              Request declined
+            </h4>
+          </template>
+          <template v-else />
+        </div>
+      </a-col>
+    </a-row>
+    <a-row>
+      <div>{{ "Request message: " + visit.request_msg }}</div>
+      <div>{{ "Response message: " + visit.response_msg }}</div>
+    </a-row>
   </a-card>
 </template>
 
@@ -123,8 +166,11 @@ export default {
     }
   },
   computed: {
+    course_group() {
+      return this.seminar.course_group;
+    },
     course() {
-      return this.seminar.course_group.course;
+      return this.course_group.course;
     }
   },
   methods: {
@@ -160,27 +206,13 @@ a {
   border-radius: 50%;
   margin: 5px;
 }
-.module-code {
-  font-size: 12px;
-  color: rgba(0, 0, 0, 0.54);
+.status-icon {
+  font-size: 20px;
   padding-right: 10px;
-}
-.seminar-title {
-  font-size: 16px;
-  font-weight: bold;
 }
 .request-status {
-  font-size: 12px;
-  padding-left: 5px;
-  padding-right: 10px;
-}
-.pending {
-  color: #ffb74d;
-}
-.accepted {
-  color: #81c784;
-}
-.declined {
-  color: #e57373;
+  font-size: 16px;
+  font-family: "Lato", sans-serif;
+  font-weight: bold;
 }
 </style>
