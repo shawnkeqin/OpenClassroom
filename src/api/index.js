f0e4ca79196206b0a24e3838c7f45c602b154969
jeremyyew <yewern@gmail.com>
2020-07-08 14:01:53 +0800

    Add env file, use jwt secret.

--- src/api/index.js
+++ src/api/index.js
@@ -1,20 +1,29 @@
 var passport = require("passport"),
   bodyParser = require("body-parser"),
   express = require("express"),
-  LdapStrategy = require("passport-ldapauth");
+  LdapStrategy = require("passport-ldapauth"),
+  jwt = require("jsonwebtoken"),
+  moment = require("moment");
 
 var api = express.Router();
-var OPTS = {
-  server: {
-    url: "ldap://localhost:10389",
-    bindDN: "cn=jeremy,ou=users,dc=example,dc=com",
-    bindCredentials: "jeremyyew",
-    searchBase: "ou=users,dc=example,dc=com",
-    searchFilter: "(cn=jeremy)",
-    reconnect: true
-  }
+
+var getLDAPConfiguration = function(req, callback) {
+  process.nextTick(function() {
+    var opts = {
+      server: {
+        url: process.env.LDAP_URL,
+        bindDn: `cn=${req.body.username},` + process.env.LDAP_BIND_DN_SUFFIX,
+        bindCredentials: `${req.body.password}`,
+        searchBase: process.env.LDAP_SEARCH_BASE,
+        searchFilter: `(cn=${req.body.username})`,
+        reconnect: true
+      }
+    };
+    callback(null, opts);
+  });
 };
-passport.use(new LdapStrategy(OPTS));
+
+passport.use(new LdapStrategy(getLDAPConfiguration));
 
 api.get("/", function(req, res) {
   res.send("API home");
@@ -25,7 +34,36 @@ api.post("/login", (req, res, next) => {
     console.log(err);
     console.log(user);
     console.log(info);
-    res.status(401).send(info);
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      res.status(401).json({
+        success: false,
+        message: "info"
+      });
+    } else {
+      const payload = {
+        exp: moment()
+          .add(30, "seconds")
+          .unix(),
+        "https://hasura.io/jwt/claims": {
+          "x-hasura-allowed-roles": ["user"],
+          "x-hasura-default-role": "user",
+          "x-hasura-user-id": "hihi"
+        }
+      };
+      jwt.sign(payload, process.env.JWT_SECRET, (err, token) => {
+        if (err) {
+          console.log(err);
+          res.send(err);
+        }
+        res.json({
+          success: true,
+          token: "Bearer " + token
+        });
+      });
+    }
   })(req, res, next);
 });
 
@@ -40,3 +78,5 @@ module.exports = app => {
 //   console.log("Time: ", Date.now());
 //   next();
 // });
+
+// {"type": "RS512", "key": ";"}
