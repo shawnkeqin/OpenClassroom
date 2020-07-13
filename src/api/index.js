11def29dfeacc6593b501e8988ec6e33490b0ba1
jeremyyew <yewern@gmail.com>
2020-07-14 01:57:36 +0800

    Try cors package again.

--- src/api/index.js
+++ src/api/index.js
@@ -101,7 +101,7 @@ api.use(function(req, res, next) {
   next();
 });
 module.exports = app => {
-  app.use(cors);
+  app.use(cors());
   app.use(bodyParser.json());
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
