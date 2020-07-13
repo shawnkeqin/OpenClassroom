e83ad8391e8ea31dfedb6336fb7aaff298f8dcd8
jeremyyew <yewern@gmail.com>
2020-07-13 18:38:18 +0800

    Accept CORS

--- src/api/index.js
+++ src/api/index.js
@@ -93,6 +93,14 @@ api.use(bodyParser.urlencoded({ extended: false }));
 api.use(passport.initialize());
 module.exports = app => {
   app.use(bodyParser.json());
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
   app.use("/api", api);
   app.use("/notifs", notifsRouter);
 };
