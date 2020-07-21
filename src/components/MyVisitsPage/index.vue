bc274a53e1f134136d6ed7606e23f17c1a373b57
Dan Tran <picasdan99@gmail.com>
2020-07-21 15:50:31 +0800

    Refactor SeminarVisitRequestCard component and fix bug related to state update for visits

--- src/components/MyVisitsPage/index.vue
+++ src/components/MyVisitsPage/index.vue
@@ -2,13 +2,22 @@
   <div class="page-wrapper">
     <h1>My Visits</h1>
     <div style="display: flex;">
-      <div v-if="seminarsToVisit.length">
+      <!-- <div v-if="seminarsToVisit.length">
         <SeminarRequestCard
           v-for="seminarWithVisits in seminarsToVisit"
           :seminar="seminarWithVisits.seminar"
           :visits="seminarWithVisits.visits"
           :isMessagesVisible="true"
           :key="seminarWithVisits.seminar.id"
+        />
+      </div> -->
+      <div v-if="myVisits.length">
+        <SeminarVisitRequestCard
+          v-for="visit in myVisits"
+          :seminar="visit.seminar"
+          :visit="visit"
+          :isMessagesVisible="true"
+          :key="visit.id"
         />
       </div>
       <div v-else>
@@ -52,14 +61,16 @@
 </template>
 
 <script>
-import SeminarRequestCard from "../viewSeminars/SeminarRequestCard";
+// import SeminarRequestCard from "../viewSeminars/SeminarRequestCard";
+import SeminarVisitRequestCard from "@/components/SeminarVisitRequestCard";
 import queries from "@/graphql/queries.gql";
 import store from "@/store";
 
 export default {
   name: "observelog",
   components: {
-    SeminarRequestCard
+    // SeminarRequestCard
+    SeminarVisitRequestCard
   },
   data() {
     return {
@@ -78,28 +89,28 @@ export default {
       update: data => data.visit
     }
   },
-  computed: {
-    seminarsToVisit() {
-      if (!this.myVisits.length) return [];
-      return this.myVisits.reduce((acc, cur) => {
-        const cur_seminar = cur.seminar;
-        const idx =
-          acc.length === 0
-            ? -1
-            : acc.findIndex(
-                seminarWithVisits =>
-                  seminarWithVisits.seminar.id === cur.seminar_id
-              );
-        if (idx === -1) {
-          acc.push({ seminar: cur_seminar, visits: [cur] });
-        } else {
-          const curr_seminarWithVisits = acc[idx];
-          curr_seminarWithVisits.visits.push(cur);
-        }
-        return acc;
-      }, []);
-    }
-  }
+  // computed: {
+  //   seminarsToVisit() {
+  //     if (!this.myVisits.length) return [];
+  //     return this.myVisits.reduce((acc, cur) => {
+  //       const cur_seminar = cur.seminar;
+  //       const idx =
+  //         acc.length === 0
+  //           ? -1
+  //           : acc.findIndex(
+  //               seminarWithVisits =>
+  //                 seminarWithVisits.seminar.id === cur.seminar_id
+  //             );
+  //       if (idx === -1) {
+  //         acc.push({ seminar: cur_seminar, visits: [cur] });
+  //       } else {
+  //         const curr_seminarWithVisits = acc[idx];
+  //         curr_seminarWithVisits.visits.push(cur);
+  //       }
+  //       return acc;
+  //     }, []);
+  //   }
+  // }
 };
 </script>
 
