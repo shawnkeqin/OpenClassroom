504a6980cad320108f3a6030f487407c4d7f6eb1
Dan Tran <picasdan99@gmail.com>
2020-07-22 15:40:21 +0800

    Implement filter visits by semesters

--- src/components/MyVisitsPage/index.vue
+++ src/components/MyVisitsPage/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="page-wrapper">
+  <div>
     <h1>My Visits</h1>
     <div style="display: flex;">
       <!-- <div v-if="seminarsToVisit.length">
@@ -11,9 +11,10 @@
           :key="seminarWithVisits.seminar.id"
         />
       </div> -->
+      <!-- <div>{{ JSON.stringify(visitsBySelectedSemester )}}</div> -->
       <div v-if="myVisits.length">
         <SeminarVisitRequestCard
-          v-for="visit in myVisits"
+          v-for="visit in visitsBySelectedSemester"
           :seminar="visit.seminar"
           :visit="visit"
           :isMessagesVisible="true"
@@ -28,32 +29,43 @@
         </div>
       </div>
       <div>
-        <div style="position: sticky; top: 20px; margin: 50px">
-          <p>
-            <a @click="mapVisible = true" href="#">View campus map</a>
-          </p>
-          <a-modal
-            title="Campus map"
-            :visible="mapVisible"
-            @cancel="mapVisible = false"
-            width="60vw"
-          >
-            <template slot="footer">
-              <div />
-            </template>
-            <img
-              src="https://library.yale-nus.edu.sg/wp-content/uploads/2014/01/campus-map_Aug2015.jpg"
-              width="100%"
-            />
-          </a-modal>
-          <p>
-            CTL best practices for Peer Observation: download
-            <a
-              href="https://teaching.yale-nus.edu.sg/wp-content/uploads/sites/25/2019/04/Peer-Observation-Booklet-web-version-edited-linked.pdf#page=20"
-              target="_blank"
-              >here</a
+        <div style="position: sticky; top: 20px; margin: 45px 20px">
+          <a-card>
+            <p>
+              View campus map <a @click="mapVisible = true" href="#">here</a>
+            </p>
+            <a-modal
+              title="Campus map"
+              :visible="mapVisible"
+              @cancel="mapVisible = false"
+              width="60vw"
             >
-          </p>
+              <template slot="footer">
+                <div />
+              </template>
+              <img
+                src="https://library.yale-nus.edu.sg/wp-content/uploads/2014/01/campus-map_Aug2015.jpg"
+                width="100%"
+              />
+            </a-modal>
+            <p>
+              CTL best practices for Peer Observation: download
+              <a
+                href="https://teaching.yale-nus.edu.sg/wp-content/uploads/sites/25/2019/04/Peer-Observation-Booklet-web-version-edited-linked.pdf#page=20"
+                target="_blank"
+                >here</a
+              >
+            </p>
+            <h4>Visits by semester</h4>
+            <a-menu v-model="selected_semester">
+              <a-menu-item
+                v-for="semester in visitsBySemester"
+                :key="semester.semester_code"
+              >
+                {{ semester.semester_code }}
+              </a-menu-item>
+            </a-menu>
+          </a-card>
         </div>
       </div>
     </div>
@@ -75,6 +87,7 @@ export default {
   data() {
     return {
       myVisits: [],
+      selected_semester: [process.env.VUE_APP_SEMESTER_CODE], // to use v-model for a-menu, the data must but an array
       mapVisible: false
     };
   },
@@ -89,28 +102,57 @@ export default {
       update: data => data.visit
     }
   },
-  // computed: {
-  //   seminarsToVisit() {
-  //     if (!this.myVisits.length) return [];
-  //     return this.myVisits.reduce((acc, cur) => {
-  //       const cur_seminar = cur.seminar;
-  //       const idx =
-  //         acc.length === 0
-  //           ? -1
-  //           : acc.findIndex(
-  //               seminarWithVisits =>
-  //                 seminarWithVisits.seminar.id === cur.seminar_id
-  //             );
-  //       if (idx === -1) {
-  //         acc.push({ seminar: cur_seminar, visits: [cur] });
-  //       } else {
-  //         const curr_seminarWithVisits = acc[idx];
-  //         curr_seminarWithVisits.visits.push(cur);
-  //       }
-  //       return acc;
-  //     }, []);
-  //   }
-  // }
+  computed: {
+    visitsBySemester() {
+      if (!this.myVisits.length) return [];
+      return this.myVisits.reduce((acc, cur) => {
+        const cur_semester_code = cur.seminar.course_group.course.semester_code;
+        const idx =
+          acc.length === 0
+            ? -1
+            : acc.findIndex(
+                semesterWithVisits =>
+                  semesterWithVisits.semester_code === cur_semester_code
+              );
+        if (idx === -1) {
+          acc.push({ semester_code: cur_semester_code, visits: [cur] });
+        } else {
+          acc[idx].visits.push(cur);
+        }
+        return acc;
+      }, []);
+    },
+    visitsBySelectedSemester() {
+      return this.visitsBySemester.find(
+        semester => semester.semester_code === this.selected_semester[0]
+      ).visits;
+    }
+    //   seminarsToVisit() {
+    //     if (!this.myVisits.length) return [];
+    //     return this.myVisits.reduce((acc, cur) => {
+    //       const cur_seminar = cur.seminar;
+    //       const idx =
+    //         acc.length === 0
+    //           ? -1
+    //           : acc.findIndex(
+    //               seminarWithVisits =>
+    //                 seminarWithVisits.seminar.id === cur.seminar_id
+    //             );
+    //       if (idx === -1) {
+    //         acc.push({ seminar: cur_seminar, visits: [cur] });
+    //       } else {
+    //         const curr_seminarWithVisits = acc[idx];
+    //         curr_seminarWithVisits.visits.push(cur);
+    //       }
+    //       return acc;
+    //     }, []);
+    //   }
+  },
+  methods: {
+    handleSelectSemester(semester_code) {
+      this.selected_semester = semester_code;
+    }
+  }
 };
 </script>
 
