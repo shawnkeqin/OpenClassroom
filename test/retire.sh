e2122d389c2c8a0347bf7df9cf2d7294a90793a4
jeremyyew <yewern@gmail.com>
2020-08-04 12:39:58 +0800

    Add test folder

---
+++ test/retire.sh
@@ -0,0 +1,7 @@
+rm ../node_modules/tinycolor2/demo/jquery-1.9.1.js
+rm ../node_modules/winchan/complex_example/jquery.js
+rm ../node_modules/winchan/example/jquery.js
+rm ../node_modules/winchan/test/qunit/jquery.js
+rm ../node_modules/winchan/test/stress/jquery.js
+rm ../node_modules/await-to-js/dist/docs/assets/js/main.js
+retire > retire_output.txt
