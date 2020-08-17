3245f3e9c4aae2c3931620148df048b29d08c1a9
jeremyyew <yewern@gmail.com>
2020-08-17 13:12:51 +0800

    use env var for db pwd

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 docker run -d -p 8080:8080 --net=host \
-       -e HASURA_GRAPHQL_DATABASE_URL="postgres://hasurauser:<>@localhost/open_classroom" \
+       -e HASURA_GRAPHQL_DATABASE_URL="postgres://hasurauser:$DB_PASSWORD@localhost/open_classroom" \
        -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
        -e HASURA_GRAPHQL_DEV_MODE=true \
        hasura/graphql-engine:v1.3.0
